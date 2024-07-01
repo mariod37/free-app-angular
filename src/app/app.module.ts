7612f092ecd5d50c8beff5f5525ee10faab68b17
mario da silva <mariodasilva201215@gmail.com>
2024-06-30 23:24:59 -0300

    primer comit freelancer angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { SectionHeroComponent } from './section-hero/section-hero.component';
+import { MainComponent } from './main/main.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    SectionHeroComponent,
+    MainComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/section-hero/section-hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-hero',
+  templateUrl: './section-hero.component.html',
+  styleUrl: './section-hero.component.css'
+})
+export class SectionHeroComponent {
+
+}
